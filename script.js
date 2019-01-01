505004f7db2b028dff7e87bd6afe9fe33fd347ec
Daniel <djbrior@gmail.com>
2019-01-01 02:01:42 -0500

    Add recent games history. Add section for stats but incomplete

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 var computerScore, playerScore;
 var over;
 var change;
+var record = [];
+var totalGames = 0;
+var specificWins = {
+    "Rock": 0,
+    "Paper": 0,
+    "Scissors": 0
+}
 setup();
 
 
@@ -18,7 +25,7 @@ function computerPlay() {
     return(move);
 }
 
-function playRound(computerSelection, playerSelection){
+function playRound(computerSelection, playerSelection) {
 
     if(!over){
         var comp = computerSelection.toLowerCase();
@@ -64,12 +71,19 @@ function playRound(computerSelection, playerSelection){
             retVal = "Tie! " + playerSelection + " and " + computerSelection.toLowerCase() + ".";
         }
 
+        updateRecord(playerSelection,computerSelection,result);
+
         document.getElementById("output").textContent = retVal;      
         console.log(retVal);
         console.log("\n");
 
         document.getElementById("player-score").textContent = playerScore;
         document.getElementById("computer-score").textContent = computerScore;
+
+        document.getElementById("rock-wins").textContent = specificWins["Rock"];
+        document.getElementById("paper-wins").textContent = specificWins["Paper"];
+        document.getElementById("scissors-wins").textContent = specificWins["Scissors"];
+
         game();
         if(!over){
             change = window.setTimeout(resetOutput,2000);
@@ -79,15 +93,15 @@ function playRound(computerSelection, playerSelection){
     
 }
 
-function resetOutput(){
+function resetOutput() {
     document.getElementById("output").textContent = "Rock, Paper, or Scissors?";
 }
 
-function setOutput(input){
+function setOutput(input) {
     document.getElementById("output").textContent = input;
 }
 
-function game(){
+function game() {
     if(!(playerScore < 5 && computerScore < 5)){
         over = true;
         if(playerScore > computerScore){
@@ -107,7 +121,8 @@ function game(){
     }
 }
 
-function reset(){
+function reset() {
+    console.log(record);
     playerScore = 0;
     computerScore = 0;
     document.getElementById("player-score").textContent = playerScore;
@@ -121,7 +136,7 @@ function reset(){
     over = false;
 }
 
-function setup(){
+function setup() {
     over = false;
     playerScore = 0;
     computerScore = 0;
@@ -141,6 +156,42 @@ function setup(){
             })
         }
     });
+}
+
+function updateRecord(playerSelection,computerSelection,result) {
+    var table = document.getElementById("games");
+    var row = document.createElement("tr");
+
+    var outcome;
+    switch(result) {
+        case 1:
+            outcome = "Win";
+            break;
+        case 0:
+            outcome = "Loss";
+            break;
+        case -1:
+            outcome = "Tie";
+            break;
+    }
+    var outcomeElem = document.createElement("td");
+    outcomeElem.appendChild(document.createTextNode(outcome));
+    row.appendChild(outcomeElem);
+
+    var playerElem = document.createElement("td");
+    playerElem.appendChild(document.createTextNode(playerSelection));
+    row.appendChild(playerElem);
+
+    var computerElem = document.createElement("td");
+    computerElem.appendChild(document.createTextNode(computerSelection));
+    row.appendChild(computerElem);
+
+    table.appendChild(row);
+
+
+    if(result == 1){
+        specificWins[playerSelection] += 1;
+    }
 }
 
 
